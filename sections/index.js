6c46cea000463430d6aca52c510f55f763875e40
Johntor12 <arkanantafirjatullahariza@mail.ugm.ac.id>
2025-07-22 21:20:01 +0700

    First Initial Commit website sebatik

--- sections/index.js
+++ sections/index.js
@@ -1,4 +1,2 @@
-export { default as AdjacentPosts } from './AdjacentPosts';
-export { default as FeaturedPosts } from './FeaturedPosts';
-export { default as Gallery } from './Gallery';
-
+export { default as AdjacentPosts } from "./AdjacentPosts";
+export { default as FeaturedPosts } from "./FeaturedPosts";
